f8ed64265f4f3b6a0ef5b8104112e408ec6518ac
Vadym Rudenko <104729209+Rudenvad@users.noreply.github.com>
2025-01-19 13:53:21 +0100

    .NET Projects created with first code problem
    1. CodeProblems project introduced;
    2. CodeProblemsTests project introduced;
    3. MergeSortedArray Problem introduced along with solution and test;

---
+++ CodeProblems/MergeSortedArray/MergeSortedArraySolution.cs
@@ -0,0 +1,48 @@
+﻿namespace CodeProblems.MergeSortedArray;
+
+public class MergeSortedArraySolution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        #region Pointers initialization
+        // Pointer for the last valid element in nums1
+        int nums1Pointer = m - 1;
+
+        // Pointer for the last element in nums2
+        int nums2Pointer = n - 1;
+
+        // Pointer for the position where the next largest element should be placed
+        int mergePointer = m + n - 1;
+        #endregion Pointers initialization
+
+        // Merge nums1 and nums2 starting from the largest elements
+        while (nums1Pointer >= 0 && nums2Pointer >= 0)
+        {
+            // Compare the current largest elements from both arrays
+            if (nums1[nums1Pointer] > nums2[nums2Pointer])
+            {
+                // Place the larger element at the merge position
+                nums1[mergePointer] = nums1[nums1Pointer];
+                nums1Pointer--;
+            }
+            else
+            {
+                // Place the larger element from nums2 at the merge position
+                nums1[mergePointer] = nums2[nums2Pointer];
+                nums2Pointer--;
+            }
+            // Move the merge pointer to the next position
+            mergePointer--;
+        }
+
+        // If any elements remain in nums2, copy them into nums1
+        while (nums2Pointer >= 0)
+        {
+            nums1[mergePointer] = nums2[nums2Pointer];
+            nums2Pointer--;
+            mergePointer--;
+        }
+
+        // Note: Remaining elements in nums1 are already in place, so no need to handle them
+    }
+}
---
+++ CodeProblemsTests/MergeSortedArray/MergeSortedArraySolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using CodeProblems.MergeSortedArray;
+
+namespace CodeProblemsTests.MergeSortedArray;
+
+public class MergeSortedArraySolutionTests
+{
+    [Theory]
+    [MemberData(nameof(GetTestCases))]
+    public void Merge_TwoArrays_ResultsMergedArray(int[] nums1, int m, int[] nums2, int n, int[] expected)
+    {
+        // Arrange
+        var solution = new MergeSortedArraySolution();
+
+        // Act
+        solution.Merge(nums1, m, nums2, n);
+
+        // Assert
+        Assert.Equal(expected, nums1);
+    }
+
+    public static IEnumerable<object[]> GetTestCases()
+    {
+        yield return new object[] { new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 } };
+        yield return new object[] { new int[] { 1, 0, 0, 0 }, 1, new int[] { 2, 3, 4 }, 3, new int[] { 1, 2, 3, 4 } };
+        yield return new object[] { new int[] { 0 }, 0, new int[] { 1 }, 1, new int[] { 1 } };
+        yield return new object[] { new int[] { 1 }, 1, new int[] { }, 0, new int[] { 1 } };
+        yield return new object[] { new int[] { 2, 0 }, 1, new int[] { 1 }, 1, new int[] { 1, 2 } };
+        yield return new object[] { new int[] { 4, 5, 6, 0, 0, 0 }, 3, new int[] { 1, 2, 3 }, 3, new int[] { 1, 2, 3, 4, 5, 6 } };
+    }
+}
